c168f0576965c21e351ce8134a1e051e96dd45c6
suleman-suraj <sulemansuraj1233@gmail.com>
2022-03-17 17:04:09 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import writers from "./writers";
+
+function App() {
+  return (
+    <div>
+    <h1>writers Profile</h1>
+    <div className = "container">
+    {
+      writers.map((writer) =>(
+        <div key={writer.id} className="card">
+        <img src={`images/${writer.avatar}.png`}
+        height = '300px'
+        width='300px' alt={writer.img}
+        />
+        <div className="textGroup">
+        <h3>{writer.name}</h3>
+        <p>{writer.email}</p>
+        <p>{writer.phone}</p>
+        
+        </div>
+        </div>
+      )
+
+      )
+    }
+    </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/writers.js
@@ -0,0 +1,39 @@
+const writers=[
+    {
+        id:1,
+        name: "Donnish Hardy",
+        avatar:"audre_hardy",
+        email: "hardy@gmail.com",
+        phone: "0247992364"
+    },
+    {
+        id:2,
+        name: "Donnish Hardy",
+        avatar:"belle_hooks",
+        email: "hardy@gmail.com",
+        phone: "0202022415"
+    },
+    {
+        id:3,
+        name: "Donnish Hardy",
+        avatar:"du_bois",
+        email: "hardy@gmail.com",
+        phone: "0270165233"
+    },
+    {
+        id:4,
+        name: "Donnish Hardy",
+        avatar:"james_baldwin",
+        email: "hardy@gmail.com",
+        phone: "0503344565"
+    },
+    {
+        id:5,
+        name: "Donnish Hardy",
+        avatar:"maya_angelou",
+        email: "hardy@gmail.com",
+        phone: "0540000000"
+    }
+]
+
+export default writers;
